ddc12739345625a43b44a02b213ed451576e393f
arnob999 <arnob196@gmail.com>
2023-03-20 17:15:03 +0600

    firebase initialized

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm()
+
+    const handleLogin = data => {
+        console.log(data)
+    }
     return (
-        <div>
-            Login
+        <div className='h-[800px]  flex justify-center items-center'>
+            <div className='w-96 p-9'>
+                <h2 className='text-xl text-center'>
+                    Login
+                </h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className='label'>
+                            <span className='label-text'>
+                                Email
+                            </span>
+                        </label>
+                        <input className="input input-bordered w-full max-w-xs" type="text" {...register("email")} />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className='label'>
+                            <span className='label-text'>
+                                Password
+                            </span>
+                        </label>
+                        <input className="input input-bordered w-full max-w-xs" type="password" {...register("password")} />
+
+                        <label className='label'>
+                            <span className='label-text'>
+                                Forget Password?
+                            </span>
+                        </label>
+                    </div>
+                    <input className='btn btn-accent w-full' value="Login" type="submit" />
+                </form>
+                <p>
+                    New to Doctor's Portal? <Link className='text-secondary' to={"/signup"}>Create New Account</Link>
+                </p>
+                <div className="divider">OR</div>
+                <button className='uppercase w-full btn btn-outline'>
+                    Continue With GOOGLE
+                </button>
+            </div>
         </div>
     );
 };
