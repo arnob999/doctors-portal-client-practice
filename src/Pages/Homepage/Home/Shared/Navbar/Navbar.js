f165f82b18b94d67ee7a4edde52182ba24cfd242
arnob999 <arnob196@gmail.com>
2023-03-24 01:05:24 +0600

    conditional rendering

--- src/Pages/Homepage/Home/Shared/Navbar/Navbar.js
+++ src/Pages/Homepage/Home/Shared/Navbar/Navbar.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../../../contexts/AuthProvider';
 
 const Navbar = () => {
+    const { user } = useContext(AuthContext)
     const menuItems = <>
         <li><Link to={'/'}>Home</Link></li>
         <li><Link to={'/appointment'}>Appointment</Link></li>
         <li><Link to={'/about'}>About</Link></li>
-        <li><Link to={'/login'}>Login</Link></li>
-        <li><Link to={'/signup'}>SignUp</Link></li>
+        {
+            user?.uid ? <li><Link to={'/login'}>Sign Out</Link></li> : <>
+                <li><Link to={'/login'}>Login</Link></li>
+                <li><Link to={'/signup'}>SignUp</Link></li>
+            </>
+        }
+
     </>
     return (
         <div className="navbar bg-base-100 flex justify-between">
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,12 +1,12 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import app from '../firebase/firebase.config'
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
 
 
 export const AuthContext = createContext()
 const auth = getAuth(app)
 const AuthProvider = ({ children }) => {
-
+    const [user, setUser] = useState(null)
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
@@ -17,9 +17,22 @@ const AuthProvider = ({ children }) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    //observer
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('User Observing');
+            setUser(currentUser)
+        })
+
+
+        return () => unSubscribe();
+    }, [])
+
+
     const authInfo = {
         createUser,
-        signIn
+        signIn,
+        user
     }
     return (
         <AuthContext.Provider value={authInfo}>
