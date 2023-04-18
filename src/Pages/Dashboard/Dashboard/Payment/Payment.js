245fa50b8efe7a1f2d2a6fe2fdc4d507cd27c6f7
arnob999 <arnob196@gmail.com>
2023-04-18 20:26:38 +0600

    final push

--- src/Pages/Dashboard/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Dashboard/Payment/Payment.js
@@ -3,20 +3,20 @@ import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData, useNavigation } from 'react-router-dom';
 import CheckoutForm from './CheckoutForm';
-import Loading from '../../../Shared/Loading/Loading';
+// import Loading from '../../../Shared/Loading/Loading';
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK)
 const Payment = () => {
     const booking = useLoaderData()
 
     //beware of NAVIGATION it is not navigate
-    const navigation = useNavigation()
+    // const navigation = useNavigation()
     const { treatment, price, appointmentDate, slot } = booking
 
     //to show a loading spinner when using default Loader of React-Router-Dom
-    if (navigation.state === "loading") {
-        return <Loading />
-    }
+    // if (navigation.state === "loading") {
+    //     return <Loading />
+    // }
 
     return (
         <div>
